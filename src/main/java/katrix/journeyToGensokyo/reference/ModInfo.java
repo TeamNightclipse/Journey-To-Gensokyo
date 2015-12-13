424ba71e279849748320e9312059686170492e95
Katrix- <katrix97@hotmail.com>
2015-12-13 01:11:52 +0100

    Properties for gradle
    There, now I won't forget to change it somewhere

--- src/main/java/katrix/journeyToGensokyo/reference/ModInfo.java
+++ src/main/java/katrix/journeyToGensokyo/reference/ModInfo.java
@@ -12,7 +12,7 @@ package katrix.journeyToGensokyo.reference;
 public class ModInfo {
 	public static final String MODID = "journeyToGensokyo";
 	public static final String NAME = "Journey To Gensokyo";
-	public static final String VERSION = "0.8.0";
+	public static final String VERSION = "@VERSION@";
 	public static final String CLIENT_PROXY_CLASS = "katrix.journeyToGensokyo.client.ClientProxy";
 	public static final String SERVER_PROXY_CLASS = "katrix.journeyToGensokyo.CommonProxy";
 	public static final String GUI_FACTORY_CLASS = "katrix.journeyToGensokyo.client.gui.GuiFactory";
